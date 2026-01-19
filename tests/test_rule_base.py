137abf9aee674a9bc53ff36c7229112441dfe507
uc-steffenr <steffenr@mail.uc.edu>
2026-01-18 21:16:19 -0500

    starting to write rule base and stats tests

---
+++ tests/test_rule_base.py
@@ -0,0 +1,16 @@
+"""Tests fuzzy variable class."""
+import unittest
+
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+
+from flex.fiss import RuleBase, RuleStats, update_rule_stats, mf_usage_from_stats
+
+
+class TestRuleBase(unittest.TestCase):
+    ...
+
+
+class TestRuleStats(unittest.TestCase):
+    ...
