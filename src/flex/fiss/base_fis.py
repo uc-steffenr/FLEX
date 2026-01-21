402ffa94618801dfaf489e42a1b7d4a1f113f4b7
uc-steffenr <steffenr@mail.uc.edu>
2026-01-21 11:20:12 -0500

    finished base fis class

--- src/flex/fiss/base_fis.py
+++ src/flex/fiss/base_fis.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import abc
-from typing import Literal, Sequence, Tuple
+from typing import Literal, Self, Sequence
 
 import jax.numpy as jnp
 import equinox as eqx
@@ -12,46 +12,67 @@ from ..fuzzy_variable import FuzzyVariable
 from ..utils.types import Array
 
 
-# TODO: there should be a mechanism for multi-output in a fis (reuses rule base, different consequents)
 class BaseFIS(eqx.Module, abc.ABC):
     input_vars: tuple[FuzzyVariable, ...]
 
     rb: RuleBase = eqx.field(static=True)
     n_mfs_max: int = eqx.field(static=True)
     name: str = eqx.field(static=True, default="fis", kw_only=True)
+    eps: float = eqx.field(static=True, default=1e-6, kw_only=True)
 
     @classmethod
     def init(
         cls,
         input_vars: tuple[FuzzyVariable, ...],
         *,
-        antecedents: Sequence[Sequence[Tuple[int, int]]]|None = None,
-        name: str="fis",
+        antecedents: Sequence[Sequence[tuple[int, int]]]|None = None,
         tnorm: Literal["prod", "min"] = "prod",
-    ) -> "BaseFIS":
+        name: str = "fis",
+        eps: float = 1e-6,
+    ) -> Self:
+        input_vars = tuple(input_vars)
+
         n_mfs_per_var = [fv.n_mfs for fv in input_vars]
         n_mfs_max = max(n_mfs_per_var)
 
         if antecedents is None:  # assume dense rulebase
             rb = RuleBase.dense(n_mfs_per_var=n_mfs_per_var, tnorm=tnorm)
         else:
-            assert antecedents.shape[1] == len(input_vars)
             rb = RuleBase.sparse(n_vars=len(input_vars), rules=antecedents, tnorm=tnorm)
 
-        return cls(input_vars=input_vars, rb=rb, n_mfs_max=n_mfs_max, name=name)
+        return cls(input_vars=input_vars, rb=rb, n_mfs_max=n_mfs_max, name=name, eps=eps)
 
     def fuzzify(self, x: Array) -> Array:
+        """Fuzzifies input values.
+
+        Parameters
+        ----------
+        x : Array
+            Input values of shape (..., n_inps).
+
+        Returns
+        -------
+        Array
+            Membership functions values, of shape (..., n_inps, n_mfs_max).
+        """
         x = jnp.asarray(x)
+
+        eqx.error_if(
+            x,
+            x.shape[-1] != self.n_inps,
+            f"x last dimension must be {self.n_inpts}."
+        )
 
         mus = []
         for i, var in enumerate(self.input_vars):
             mu = var(x[..., i])
-            pad_width = self.n_mfs_max - mu.shape[-1]
+            pad = self.n_mfs_max - mu.shape[-1]
 
-            if pad_width > 0:
+            if pad > 0:
+                pad_spec = ((0, 0),) * (mu.ndim - 1) + ((0, pad),)
                 mu = jnp.pad(
                     mu,
-                    pad_width=((0, 0), (0, pad_width)),
+                    pad_width=pad_spec,
                     mode="constant",
                     constant_values=0.0,
                 )
@@ -69,3 +90,7 @@ class BaseFIS(eqx.Module, abc.ABC):
         w = self.rb.fire(mus)
 
         return self.defuzzify(w, x)
+
+    @property
+    def n_inps(self) -> int:
+        return len(self.input_vars)
